b5c8a35bdd4188d6036a955069e6b5e10335c951
pallavisaxena0245 <pallavisaxena0245@gmail.com>
2024-12-17 16:52:30 +0530

    added test network

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
 module.exports = {
-  solidity: "0.8.28",
+   solidity: "0.8.0",
+   networks: {
+      gochain_testnet: {
+         url: "http://127.0.0.1:8545", // GoChain Testnet RPC
+         accounts: [process.env.PRIVATE_KEY], // Your private key
+         chainId: 31337, // GoChain Testnet chain ID
+      },
+   },
 };
